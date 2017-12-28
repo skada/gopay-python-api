a9deb37c772fcbc1f31095d76fff9194f9fe37bd
Jakub Skaryd <skaryd@gmail.com>
2017-12-28 09:54:44 +0100

    Remove the pointless pandoc dependency

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
-from pypandoc import convert
 
 setup(
     name='gopay',
     version='1.2.0',
     description='GoPay\'s Python SDK for Payments REST API',
-    long_description=convert('README.md', 'rst'),
+    long_description='README.md',
     url='https://github.com/gopaycommunity/gopay-python-sdk',
     author='GoPay',
     author_email='integrace@gopay.cz',
